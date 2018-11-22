b8a2bef08bdaffa9b0e40041de56bec94ad65cb5
LakeMirror <hugysuccez@163.com>
2018-11-22 17:49:47 +0800

    首页大致写完

--- pages/mainPage/main.js
+++ pages/mainPage/main.js
@@ -8,7 +8,43 @@ Page({
       "http://tp.yiaedu.com/showimg.php?url=http://uploads.xuexila.com/allimg/1703/867-1F330164643.jpg",
       "http://www.17qq.com/img_qqtouxiang/76490995.jpeg"
     ],
-    searchKey: ''
+    searchKey: '',
+    shopDesc: {
+      shopName: '超简单小店',
+      shopDesc: '超简单， 让购物超简单',
+      shopLoc: '华夏村33号',
+      shopImage: '',
+      fans: 177,
+      buyPoint: 1733
+    },
+    goodsList: [
+      {
+        goodsId: '123456',
+        goodsImage: "http://pic.58pic.com/58pic/14/03/02/58V58PICTFk_1024.jpg",
+        goodsName: '哈密瓜',
+        goodsDesc: '【秒杀】',
+        goodsWeight: '4斤土0.3',
+        goodsLable: ['限时卖', '酸甜可口 水分超足'],
+        goodsPrice: '7.98',
+        goodsSaleNum: '99',
+        goodsRemain: '1',
+        preSaleTime: '11-21 20:00 - 11-22 19:00',
+        goodsPickTime: '11-23 12:05'
+      },
+      {
+        goodsId: '123456',
+        goodsImage: "http://pic.58pic.com/58pic/14/03/02/58V58PICTFk_1024.jpg",
+        goodsName: '哈密瓜',
+        goodsDesc: '【秒杀】',
+        goodsWeight: '4斤土0.3',
+        goodsLable: ['限时卖', '酸甜可口 水分超足'],
+        goodsPrice: '7.98',
+        goodsSaleNum: '99',
+        goodsRemain: '1',
+        preSaleTime: '11-21 20:00 - 11-22 19:00',
+        goodsPickTime: '11-23 12:05'
+      }
+    ]
   },
 
   /**
