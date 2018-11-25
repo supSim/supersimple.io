64172bcd877d1f1e723d2be6513e0a515d9b79a1
unknown <hugy@raiyee.com>
2018-11-25 20:33:16 +0800

    personal

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,65 +1,73 @@
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    
+    userInfo: {},
+    motto: 'Hello World',
+    // orderItems
+    orderItems: [
+      {
+        typeId: 0,
+        name: '待付款',
+        url: 'bill',
+        imageurl: 'icon-daifukuan',
+      
+      },
+      {
+        typeId: 1,
+        name: '待发货',
+        url: 'bill',
+        imageurl: 'icon-daifahuo',
+      },
+      {
+        typeId: 2,
+        name: '已发货',
+        url: 'bill',
+        imageurl: 'icon-yifahuo'
+      },
+      {
+        typeId: 3,
+        name: '签收',
+        url: 'bill',
+        imageurl: 'icon-qianshou'
+      }
+    ],
+  },
+  //事件处理函数
+  toOrder: function () {
+    wx.navigateTo({
+      url: '../order/order'
+    })
+  },
+  toPointmall: function () {
+    wx.navigateTo({
+      url: '../pointMall/pointMall'
+    })
+  },
+  toCoupon: function () {
+    wx.navigateTo({
+      url: '../coupon/coupon'
+    })
+  },
+  toCheaklist: function () {
+    wx.navigateTo({
+      url: '../checkList/checkList'
+    })
+  },
+  toPersonalinfo: function () {
+    wx.navigateTo({
+      url: '../personalInfo/personalInfo'
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    console.log('personal');
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+        that.setData({
+        userInfo: userInfo
+      })
+    })
   }
 })
